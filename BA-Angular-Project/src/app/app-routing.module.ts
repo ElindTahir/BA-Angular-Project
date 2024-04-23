a3aed1e1713a62256c1a8bd62df41299ad57cc4d
ElindTahir <if21b033@technikum-wien.at>
2024-04-23 23:46:39 +0200

    Routing, navBar

--- BA-Angular-Project/src/app/app-routing.module.ts
+++ BA-Angular-Project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DatabasePageComponent } from './database-page/database-page.component';
+import { ApiFetchingPageComponent } from './api-fetching-page/api-fetching-page.component';
+import { ChartPageComponent } from './chart-page/chart-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'database-page', component: DatabasePageComponent },
+  { path: 'api-fetching-page', component: ApiFetchingPageComponent },
+  { path: 'chart-page', component: ChartPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
